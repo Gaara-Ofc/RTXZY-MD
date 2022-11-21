909e302b7a9d72753bff1012b2e8cd5e1a3bad07
Gaara Setyawan <90649022+Gaara-Ofc@users.noreply.github.com>
2022-11-21 21:56:43 +0700

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,37 +1,25 @@
-import { addExif, sticker } from '../lib/sticker.js'
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
-
-
+const { sticker } = require('../lib/sticker')
+const uploadFile = require('../lib/uploadFile')
+const uploadImage = require('../lib/uploadImage')
+let { webp2png } = require('../lib/webp2mp4')
+let fetch = require("node-fetch")
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  let stiker = false
-  try {
-    let [packname, ...author] = text.split`|`
-    author = (author || []).join('|')
     let q = m.quoted ? m.quoted : m
-    let mime = m.quoted.mimetype || ''
-    if (/webp/.test(mime)) {
-      let img = await m.quoted.download()
-      if (!img) throw 'Reply a sticker!'
-      stiker = await addExif(img, packname || '', author || '')
-    } else if (/image/.test(mime)) {
-      let imge = await m.quoted.download()
-      let outi = await uploadImage(imge)
-      stiker = await sticker(outi, packname || '', author || '')
-    } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) return m.reply('maks 10 detik!')
-      let imgo = await m.quoted.download()
-      let outu = await uploadImage(imgo)
-      stiker = await sticker( outu, packname || '', author || '')
-    }
-  } finally {
-    if (stiker) await conn.sendFile(m.chat, stiker, 'stiker.webp', '', m, false, { asSticker: true })
-    else throw `Balas stiker dengan perintah *${usedPrefix + command} <teks>|<teks>*`
-  }
+    let mime = (q.msg || q).mimetype || ''
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+try {
+      let img = await q.download()
+   
+if(!img) throw `balas gambar/video/stiker dengan perintah .stiker` 
+conn.sendImageAsSticker(m.chat , img, m, {packname: text, author: ''})
+    } catch { throw 'Gagal!, Balas Gambar/video dengan caption *.stiker*'}
 }
-handler.help = ['wm <packname>|<author>']
+handler.help = ['wm', 'watermark']
 handler.tags = ['sticker']
-handler.command = /^(s(ti(ck(erwn|wm)|k(er)?wm)|wm)|colong|wm)$/i
+handler.command = /^wm|watermark?$/i
 
-export default handler
+module.exports = handler
+
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
